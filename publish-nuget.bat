215aa724e89e9501e0e52d70c7c31efd9e85885b
JeremyWildsmith <jeremyawildsmith@gmail.com>
2023-10-15 19:07:33 -0700

    feat(build): Still trying to get this to work.

--- publish-nuget.bat
+++ publish-nuget.bat
@@ -1,3 +1,1 @@
-dotnet build --no-restore --configuration Release
-dotnet pack .\OpenMud.Mudpiler.Compiler.Project.Cli\OpenMud.Mudpiler.Compiler.Project.Cli.csproj -c Release -o out
-dotnet nuget push -s https://api.nuget.org/v3/index.json -k  $Env:NUGET_TOKEN .\out\*.nupkg
+echo released > flagnugetpush
