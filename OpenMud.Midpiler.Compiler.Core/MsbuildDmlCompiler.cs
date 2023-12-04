a4e800bfbbb8b11892d4fb4d38455d13c87a9b3f
JeremyWildsmith <jeremyawildsmith@gmail.com>
2023-12-03 22:30:50 -0800

    chore(compiler): Cleaned up some code.

--- OpenMud.Midpiler.Compiler.Core/MsbuildDmlCompiler.cs
+++ OpenMud.Midpiler.Compiler.Core/MsbuildDmlCompiler.cs
@@ -184,23 +184,12 @@ public class MsBuildDmlCompiler
         var commonTokenStream = new CommonTokenStream(lexer);
         var errorListener = new ErrorListener();
 
-        //commonTokenStream.Mark();
-        //commonTokenStream.Fill();
-
         lexer.AddErrorListener(errorListener);
 
-        //var parser = new DmlParser(commonTokenStream) { BuildParseTree = true };
         var parser = new DmlParser(commonTokenStream);
         parser.AddErrorListener(errorListener);
 
-        //Console.WriteLine($"Parse tree: {parser.dml_module().ToStringTree(parser)}");
-        //Console.WriteLine("\nTokens:");
-
-        //foreach (var token in commonTokenStream.GetTokens())
-        //    Console.WriteLine($"  {DmlLexer.DefaultVocabulary.GetSymbolicName(token.Type),-15} '{token.Text}'");
-
         var ctx = parser.dml_module();
-        Console.WriteLine($"Parse tree: {ctx.ToStringTree(parser)}");
 
 
         var visitor = new BasicModuleVisitor(lineMappings);
@@ -226,8 +215,6 @@ public class MsBuildDmlCompiler
         }
 
         var formattedCode = sb.ToString();
-
-        //var formattedCode = Formatter.Format(compilationUnit, cw);
 
         Console.WriteLine(formattedCode);
 
